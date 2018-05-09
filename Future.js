f740618e21fb5a77573150153476a7b8e23130da
Nathan Lapinski <n4t311@gmail.com>
2018-05-09 21:27:11 +0900

    adding complete function

--- Future.js
+++ Future.js
@@ -4,16 +4,35 @@
 function Future() {
     this.subscribers = [];
     this.completed = false;
+    this.value = undefined;
 }
 
 Future.prototype.ready = function(cb) {
     if (this.completed) {
-        cb();
+        cb(this.value);
     } else {
         this.subscribers.push(cb);
     }
 }
 
+Future.prototype.complete = function(val) {
+    if (this.completed) {
+        console.log('Future: Future has already been completed');
+        return;
+    }
+    this.value = val;
+    this.completed = true;
+
+    this.subscribers.forEach(f => f(this.value));
+
+    this.subscribers = null;
+}
+
+function logF(future) {
+    future.ready(v => console.log('Future log: ', v));
+}
+
 module.exports = {
-    Future: Future
+    Future: Future,
+    logF: logF
 };
--- spec/Future.spec.js
+++ spec/Future.spec.js
@@ -1,4 +1,5 @@
 const Future = require('../Future').Future;
+const logF = require('../Future').logF;
 
 describe('Future', () => {
   let future;
@@ -19,6 +20,10 @@ describe('Future', () => {
     expect(future.completed).toBe(false);
   });
 
+  it('should have an undefined value by default', () => {
+    expect(future.value).toBeUndefined();
+  });
+
   describe('ready', () => {
     it('should register a callback if it is not ready', () => {
       const cb = () => {};
@@ -30,7 +35,7 @@ describe('Future', () => {
       expect(future.subscribers.length).toBe(1);
     });
 
-    it('should call a subscriber immediatle if it has already completed', () => {
+    it('should call a subscriber immediately if it has already completed', () => {
       const cb1 = jasmine.createSpy();
 
       future.completed = true;
@@ -38,7 +43,82 @@ describe('Future', () => {
       future.ready(cb1);
 
       expect(cb1).toHaveBeenCalled();
+    });
 
+    it('should call a subscriber with the resolved value', () => {
+      const cb1 = jasmine.createSpy();
+
+      future.completed = true;
+      future.value = 'resolved';
+
+      future.ready(cb1);
+
+      expect(cb1).toHaveBeenCalledWith('resolved');
+    });
+  });
+
+  describe('complete', () => {
+    it('should complete the future with the supplied value', () => {
+      expect(future.value).toBeUndefined();
+      expect(future.completed).toBe(false);
+
+      future.complete(123);
+
+      expect(future.value).toBe(123);
+      expect(future.completed).toBe(true);
+    });
+
+    it('should not let the completed value be set more than once. It should be immutable', () => {
+      expect(future.value).toBeUndefined();
+      expect(future.completed).toBe(false);
+
+      future.complete(123);
+
+      expect(future.value).toBe(123);
+      expect(future.completed).toBe(true);
+
+      future.complete(456);
+
+      expect(future.value).toBe(123);
+      expect(future.completed).toBe(true);
+    });
+
+    it('should call all subscribers with the completed value', () => {
+      const f1 = jasmine.createSpy();
+      const f2 = jasmine.createSpy();
+
+      future.ready(f1);
+      future.ready(f2);
+
+      expect(future.value).toBeUndefined();
+      expect(future.completed).toBe(false);
+
+      future.complete(123);
+
+      expect(future.value).toBe(123);
+      expect(future.completed).toBe(true);
+      expect(f1).toHaveBeenCalledWith(123);
+      expect(f2).toHaveBeenCalledWith(123);
+    });
+
+    it('should release all subscribers after invoking them', () => {
+      const f1 = jasmine.createSpy();
+
+      future.ready(f1);
+
+      expect(future.subscribers.length).toBe(1);
+
+      future.complete(123);
+
+      expect(future.subscribers).toBeNull();
+    });
+  });
+
+  describe('logging utility', () => {
+    it('should register a logging callback on the future it receives as an argument', () => {
+      const future = jasmine.createSpyObj(Future, ['ready']);
+      logF(future);
+      expect(future.ready).toHaveBeenCalled();
     });
   });
 
