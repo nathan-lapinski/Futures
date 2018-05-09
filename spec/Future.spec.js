a8f77f0359e94259fc96105e6d1ca2bf48f2f1da
Nathan Lapinski <n4t311@gmail.com>
2018-05-09 18:47:23 +0900

    Adding Future function

--- spec/Future.spec.js
+++ spec/Future.spec.js
@@ -1,7 +1,15 @@
+const Future = require('../Future').Future;
+
 describe('Future', () => {
 
-  it('should run a test', () => {
-    expect(1).toBe(1);
+  it('should be callable as a constructor', () => {
+    const future = new Future();
+    expect(future).toBeDefined();
+  });
+
+  it('should maintain a list of registered callbacks', () => {
+    const future = new Future();
+    expect(future.subscribers).toBeDefined();
   });
 
 });
